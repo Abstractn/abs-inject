a53b5c228732e6732191795bf08de3c9d32af0a6
Abstractn <58334120+Abstractn@users.noreply.github.com>
2026-05-29 18:56:44 +0200

    Drafted wrapper method for MutationObserver and added JsDocs

--- abs-inject.js
+++ abs-inject.js
@@ -1,4 +1,9 @@
 (() => {
+  /**
+   * @param {string} query
+   * @param {function} callback
+   * @param {HTMLElement} [scopeNode]
+   */
   const waitFor = (query, callback, scopeNode = document) => {
     const queryNode = scopeNode.querySelector(query);
     if(queryNode) {
@@ -15,6 +20,27 @@
     }
   };
 
+  /**
+   * 
+   * @param {HTMLElement} node
+   * @param {(mutations: MutationRecord[], stop: () => void) => void} callback
+   * @param {MutationObserverInit} [options]
+   * @returns {() => void}
+   */
+  const onDomChanges = (node, callback, options) => {
+    const observerOptions = {
+      attributes: options?.attributes ?? true,
+      subtree: options?.subtree ?? true,
+      childList: options?.childList ?? true,
+    };
+
+    const obs = new MutationObserver((mutationList) => {
+      callback && callback(mutationList, () => obs.disconnect());
+    });
+    obs.observe(node, observerOptions);
+    return () => obs.disconnect();
+  };
+
   [
     Document,
     Element,
@@ -22,7 +48,9 @@
     Node
   ].forEach(NativeClass => {
     NativeClass.prototype.waitFor = function (query, callback) { return waitFor(query, callback, this); };
+    NativeClass.prototype.onDomChanges = function (callback, options) { return onDomChanges(this, callback, options); };
   });
 
   window.waitFor = waitFor;
+  window.onDomChanges = onDomChanges;
 })();
